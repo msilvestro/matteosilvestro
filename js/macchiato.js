7f8821362c02a2855435388167b840941d6f72cd
Matteo Silvestro <matteosilvestro@live.it>
2020-03-08 02:21:40 +0100

    completed introduction, implemented video embed

---
+++ js/macchiato.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    var personae = ['fb', 'tw', 'ig', 'lf', 'st', 'yt', 'ms', 'gh']
+    $('#la-mia-persona a').each(function() {
+        if (personae.includes($(this).attr('id'))) {
+            $(this).hover(
+                function() {
+                    $('#la-mia-persona img').css('background-color', $(this).css('color'))
+                },
+                function() {
+                    $('#la-mia-persona img').css('background-color', '')
+                })
+        }
+    })
+})
+
+function openVideo(thisdiv, videoId) {
+    // window.open('https://www.youtube.com/watch?v='+videoId, '_blank')
+    $(thisdiv).removeClass('videothumbnail')
+    $(thisdiv).addClass('videowrapper')
+    $(thisdiv).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+videoId+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+}
--- pages/introduzione.md
+++
@@ -1,5 +0,0 @@
-## Introduzione
-
-### La mia persona
-
-> Per le buone idee non serve tempo. Servono prima un sacco di pessime idee.
