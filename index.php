7793eaffee237a0d01b53fff84c2931d2cbf42b6
Matteo Silvestro <matteosilvestro@live.it>
2020-03-12 15:00:44 +0100

    created Pagemaker class

--- index.php
+++ index.php
@@ -20,15 +20,17 @@ $pages = array(
     )
 );
 
-list($page_lang, $page_title, $page_description, $cards) = get_page_data($pages);
+$page = new Pagemaker($pages);
 ?>
 <!DOCTYPE html>
-<html lang="<?= $page_lang ?>">
+<html lang="<?= $page->lang ?>">
 
     <head>
 
+        <title><?= $page->get_webpage_title() ?></title>
+
         <meta charset="UTF-8">
-        <meta name="description" content="<?= $page_description ?>">
+        <meta name="description" content="<?= $page->description ?>">
         <meta name="author" content="Matteo Silvestro">
         <meta name="generator" content="Visual Studio Code 1.42.1">
 
@@ -47,7 +49,7 @@ list($page_lang, $page_title, $page_description, $cards) = get_page_data($pages)
         <div id="layout">
 
             <div id="title"><h1>
-                <?php if ($page_lang == "en"): ?>
+                <?php if ($page->lang == "en"): ?>
                     Matteo Silvestro<span class="dim">'s page</span></h1>
                 <?php else: ?>
                     <span class="dim">La pagina di</span> Matteo Silvestro</h1>
@@ -56,42 +58,42 @@ list($page_lang, $page_title, $page_description, $cards) = get_page_data($pages)
 
             <div id="menu">
                 <ul>
-                    <?php foreach ($pages[$page_lang] as $menu_page_id => $menu_page_title): ?>
-                        <?php if ($menu_page_title == $page_title): ?>
+                    <?php foreach ($page->get_menu() as $menu_page_id => $menu_page_title): ?>
+                        <?php if ($menu_page_title == $page->title): ?>
                             <li><?= $menu_page_title ?></li>
-                        <?php elseif ($menu_page_title == $pages[$page_lang][DEFAULT_PAGE_ID]): ?>
-                            <li><a href="<?= get_lang_prefix($page_lang) ?>"><?= $menu_page_title ?></a></li>
+                        <?php elseif ($menu_page_title == $page->get_default_page()): ?>
+                            <li><a href="<?= $page->get_lang_prefix() ?>"><?= $menu_page_title ?></a></li>
                         <?php else: ?>
-                            <li><a href="<?= get_lang_prefix($page_lang).slugify($menu_page_title) ?>"><?= $menu_page_title ?></a></li>
+                            <li><a href="<?= $page->get_lang_prefix().$page->get_url($menu_page_title) ?>"><?= $menu_page_title ?></a></li>
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </ul>
             </div>
 
-            <h2><?= $page_title ?></h2>
+            <h2><?= $page->title ?></h2>
 
-            <?php foreach ($cards as $card): ?>
-            <div class="card" id="<?= slugify($card['title']) ?>">
+            <?php foreach ($page->cards as $card): ?>
+            <div class="card" id="<?= $page->get_url($card['title']) ?>">
 
                 <div class="right column">
                     <div class="title">
                         <h3><?= $card['title'] ?></h3>
                         <?php if (isset($card['date'])): ?>
-                        <span class="dim"><?= $card['date'] ?></span>
+                            <span class="dim"><?= $card['date'] ?></span>
                         <?php endif; ?>
                     </div>
-                    <?= $parsedown->text($card['right_column']) ?>
+                    <?= $page->render_markdown($card, 'right_column') ?>
                 </div>
                 <div class="left column">
-                    <?= render_video_tags($parsedown->text($card['left_column'])) ?>
+                    <?= $page->render_markdown($card, 'left_column') ?>
                 </div>
             </div>
             <?php endforeach; ?>
 
-            <?php if ($page_lang == "en"): ?>
-                <div id="footer">Matteo Silvestro 2020 — Version <abbr title="theta">&theta;</abbr> — <a href="cappuccino.css">Cappuccino</a> style — <a href="<?= get_markdown_source($page_title, $page_lang) ?>">Markdown</a> source — Versione <a href="<?= get_translated_page($pages, $page_title, $page_lang) ?>">italiana</a></div>
+            <?php if ($page->lang == "en"): ?>
+                <div id="footer">Matteo Silvestro 2020 — Version <abbr title="theta">&theta;</abbr> — <a href="cappuccino.css">Cappuccino</a> style — <a href="<?= $page->get_markdown_source() ?>">Markdown</a> source — Versione <a href="<?= $page->get_translated_page() ?>">italiana</a></div>
             <?php else: ?>
-                <div id="footer">Matteo Silvestro 2020 — Versione <abbr title="theta">&theta;</abbr> — Stile <a href="cappuccino.css">Cappuccino</a> — Sorgente <a href="<?= get_markdown_source($page_title, $page_lang) ?>">Markdown</a> — <a href="<?= get_translated_page($pages, $page_title, $page_lang) ?>">English</a> version</div>
+                <div id="footer">Matteo Silvestro 2020 — Versione <abbr title="theta">&theta;</abbr> — Stile <a href="cappuccino.css">Cappuccino</a> — Sorgente <a href="<?= $page->get_markdown_source() ?>">Markdown</a> — <a href="<?= $page->get_translated_page() ?>">English</a> version</div>
             <?php endif; ?>
 
         </div>
