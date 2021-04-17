6f7384198a7be94b0f51c6bf7a5f3711a947ad4f
Matteo Silvestro <matteosilvestro@live.it>
2021-04-17 16:45:17 +0200

    Make cube nav change pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,7 +3,9 @@ import funFile from "./fun.md"
 import educationFile from "./education.md"
 import workFile from "./work.md"
 
-export const getPage = (pageName: "index" | "fun" | "education" | "work") => {
+export type PageNames = "index" | "fun" | "education" | "work"
+
+export const getPage = (pageName: PageNames) => {
   switch (pageName) {
     case "index":
       return indexFile
