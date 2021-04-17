7f961f1a771b5b60f2712b1c62d5ee4d3d1253a4
Matteo Silvestro <matteosilvestro@live.it>
2021-04-17 13:27:13 +0200

    Improve styling and write homepage content

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
       </div>
       <div className="main">
         <div className="sidebar">
-          <div className="profile-picture">
+          <div className="profile-picture selected">
             <img src={persona_matt} alt="Profile" />
           </div>
           <nav>
@@ -31,26 +31,50 @@ const App = () => {
             />
           </nav>
         </div>
-        <div className="content">
-          <header>Projects</header>
+        <section className="content">
+          <h2>Myself</h2>
+          <blockquote>
+            Good ideas don't take time. They take a lot of bad ideas first.
+            <br />
+            —Ral Zarek
+          </blockquote>
+          <p>
+            I'm a scatterbrained wanderer always looking for new routes. My life
+            is an adagio sostenuto in A minor played on a strange instrument.
+          </p>
+          <p>
+            I seek beauty in the tidy lines of a source code, in the harmonious
+            flow of notes in a music staff and in the refined perfection of a
+            mathematical formula.
+          </p>
+          <p>
+            If you want to know me, you can discover the different sides of
+            myself though the nav cube on the left.
+          </p>
           <section>
+            <h3>Fun</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              pharetra, lacus ut cursus fermentum, nunc risus commodo dui, ut
-              porta augue lectus vel nunc. Nullam sit amet nulla gravida metus
-              pretium ullamcorper nec quis erat. Vestibulum dapibus vulputate
-              odio, ac laoreet enim consectetur ut. Vestibulum viverra augue
-              vitae dui aliquet aliquet. Donec tincidunt massa orci, ut eleifend
-              urna tempus quis. Aliquam placerat efficitur leo. Integer
-              ultricies gravida lacus, id egestas quam accumsan ut. Curabitur
-              tempus elit ut augue facilisis mollis. Cras in quam at lacus
-              pretium egestas eget sed mauris. Mauris mollis, urna sed auctor
-              porta, nibh libero viverra ante, in auctor tellus neque a neque.
-              Mauris quis tortor sed turpis pretium viverra id sit amet nisi.
-              Fusce molestie mollis risus, in gravida nisi aliquam consectetur.
+              In my free time I struggle to get bored, I have far too many
+              hobbies, like enjoying a good story (whatever the medium) or
+              building hopefully useful software.
             </p>
           </section>
-        </div>
+          <section>
+            <h3>Education</h3>
+            <p>
+              I studied mathematics because I always loved its problem-solving
+              challenge and elegance in the solutions. I then fell in love with
+              probability and statistics.
+            </p>
+          </section>
+          <section>
+            <h3>Work</h3>
+            <p>
+              I'm a developer with a mathematical background. I like to tackle a
+              problem in an abstract manner and then implement it in code.
+            </p>
+          </section>
+        </section>
       </div>
     </div>
   )
