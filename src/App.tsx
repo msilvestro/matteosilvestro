98a011173be5b82b04afb846a2d5c529b52c08d6
Matteo Silvestro <matteosilvestro@live.it>
2021-04-17 15:53:49 +0200

    Render page content via markdown page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./App.css"
 
+import markdownPage from "./pages/index.md"
 import persona_matt from "./assets/persona_matt.png"
+
+import ReactMarkdown from "react-markdown"
 
 import CubeNav from "./components/CubeNav"
 
 const App = () => {
+  const [content, setContent] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    fetch(markdownPage)
+      .then((response) => response.text())
+      .then((text) => setContent(text))
+  }, [])
+
   return (
     <div className="website">
       <div className="title">
@@ -32,48 +43,7 @@ const App = () => {
           </nav>
         </div>
         <section className="content">
-          <h2>Myself</h2>
-          <blockquote>
-            Good ideas don't take time. They take a lot of bad ideas first.
-            <br />
-            —Ral Zarek
-          </blockquote>
-          <p>
-            I'm a scatterbrained wanderer always looking for new routes. My life
-            is an adagio sostenuto in A minor played on a strange instrument.
-          </p>
-          <p>
-            I seek beauty in the tidy lines of a source code, in the harmonious
-            flow of notes in a music staff and in the refined perfection of a
-            mathematical formula.
-          </p>
-          <p>
-            If you want to know me, you can discover the different sides of
-            myself though the nav cube on the left.
-          </p>
-          <section>
-            <h3>Fun</h3>
-            <p>
-              In my free time I struggle to get bored, I have far too many
-              hobbies, like enjoying a good story (whatever the medium) or
-              building hopefully useful software.
-            </p>
-          </section>
-          <section>
-            <h3>Education</h3>
-            <p>
-              I studied mathematics because I always loved its problem-solving
-              challenge and elegance in the solutions. I then fell in love with
-              probability and statistics.
-            </p>
-          </section>
-          <section>
-            <h3>Work</h3>
-            <p>
-              I'm a developer with a mathematical background. I like to tackle a
-              problem in an abstract manner and then implement it in code.
-            </p>
-          </section>
+          {content && <ReactMarkdown>{content}</ReactMarkdown>}
         </section>
       </div>
     </div>
