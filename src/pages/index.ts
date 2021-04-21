0921c48f74eb1f5e435370b7bc32d98c95b0b784
Matteo Silvestro <matteosilvestro@live.it>
2021-04-22 00:25:59 +0200

    Add 404 error page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,16 +2,21 @@ import homeFile from "./home.md"
 import funFile from "./fun.md"
 import educationFile from "./education.md"
 import workFile from "./work.md"
+import notFoundFile from "./404.md"
 
 export type PageName = "home" | "fun" | "education" | "work"
 
-const pages = {
-  home: { file: homeFile },
-  fun: { file: funFile },
-  education: { file: educationFile },
-  work: { file: workFile },
-}
-
-export const getPage = (pageName: PageName) => {
-  return pages[pageName].file
+export const getPage = (pageName: string) => {
+  switch (pageName) {
+    case "home":
+      return homeFile
+    case "fun":
+      return funFile
+    case "education":
+      return educationFile
+    case "work":
+      return workFile
+    default:
+      return notFoundFile
+  }
 }
