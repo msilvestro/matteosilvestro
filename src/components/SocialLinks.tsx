16a799b9436ac6bd1e557ea1ebf01d04a153369a
Matteo Silvestro <matteosilvestro@live.it>
2021-04-26 00:35:00 +0200

    Add social links into the index page

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,28 @@
+import styles from "./SocialLinks.module.css"
+
+type SocialLink = {
+  name: string
+  href: string
+  color?: string
+}
+
+type Props = {
+  socialLinks: Array<SocialLink>
+}
+
+const ActiveLink = ({ socialLinks }: Props) => {
+  return (
+    <p>
+      <ul className={styles.socialLinks}>
+        {socialLinks.map((socialLink) => (
+          <li>
+            <a href={socialLink.href}>{socialLink.name}</a>
+          </li>
+        ))}
+      </ul>
+      <span>Hover on the links above to know more.</span>
+    </p>
+  )
+}
+
+export default ActiveLink
