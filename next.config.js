65871a7a49638f00a46c8019a94e8ddfa535444e
Matteo Silvestro <matteosilvestro@live.it>
2023-02-21 23:06:48 +0100

    Remove Nextjs project

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx$/,
-  options: {
-    providerImportSource: "@mdx-js/react",
-  },
-})
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-})
