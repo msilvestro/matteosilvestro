021b5650787e31c998edfff16038b89879e518b8
Matteo Silvestro <matteosilvestro@live.it>
2023-02-22 02:09:34 +0100

    Add cube nav

--- src/layout.svelte
+++ src/layout.svelte
@@ -1,4 +1,7 @@
 <script>
+  import CubeNav from './lib/CubeNav.svelte';
+  import { page } from '$app/stores';
+
   export let title;
   const currentYear = new Date().getFullYear();
 </script>
@@ -7,7 +10,23 @@
   <title>Matteo Silvestro{title ? ` | ${title}` : ''}</title>
 </svelte:head>
 
-<section><slot /></section>
-<footer>
-  ©{currentYear} Matteo Silvestro — version <abbr title="iota">&iota;</abbr>
-</footer>
+<div class="sidebar">
+  <nav>
+    <ul>
+      <li>
+        <a href="/" class:selected={$page.route.id === '/'}
+          ><div class="profile-picture">
+            <img src="/persona_matt.png" alt="My avatar" width="142" height="142" />
+          </div></a
+        >
+      </li>
+      <CubeNav size="80" fontSize="1.2em" />
+    </ul>
+  </nav>
+</div>
+<div class="content">
+  <section><slot /></section>
+  <footer>
+    ©{currentYear} Matteo Silvestro — version <abbr title="iota">&iota;</abbr>
+  </footer>
+</div>
