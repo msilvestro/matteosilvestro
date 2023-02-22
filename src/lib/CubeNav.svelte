90e45a120b91195996bdb5399723c14f53aac831
Matteo Silvestro <matteosilvestro@live.it>
2023-02-23 00:07:24 +0100

    Fix funny end tags on new line

--- src/lib/CubeNav.svelte
+++ src/lib/CubeNav.svelte
@@ -16,43 +16,43 @@
   style:height="{size * (sin30 * 2 + 1) + margin}px"
 >
   <li style:left="{size * (cos30 - 0.5) + margin * 0.5}px">
-    <a href="/fun" class:selected={$page.route.id === '/fun'}
-      ><div
+    <a href="/fun" class:selected={$page.route.id === '/fun'}>
+      <div
         class="face"
         style:height="{size}px"
         style:width="{size}px"
         style:transform="rotate(-30deg) skewX(30deg) scaleY({cos30})"
       >
         Fun
-      </div></a
-    >
+      </div>
+    </a>
   </li>
   <li style:top="{size * sin30 * 1.5 + margin}px" style:left="{-size * (1 - cos30) * 0.5}px">
-    <a href="/work" class:selected={$page.route.id === '/work'}
-      ><div
+    <a href="/work" class:selected={$page.route.id === '/work'}>
+      <div
         class="face"
         style:height="{size}px"
         style:width="{size}px"
         style:transform="skewY(30deg) scaleX({cos30})"
       >
         Work
-      </div></a
-    >
+      </div>
+    </a>
   </li>
   <li
     style:top="{size * sin30 * 1.5 + margin}px"
     style:left="{size * (cos30 * 1.5 - 0.5) + margin}px"
   >
-    <a href="/education" class:selected={$page.route.id === '/education'}
-      ><div
+    <a href="/education" class:selected={$page.route.id === '/education'}>
+      <div
         class="face"
         style:height="{size}px"
         style:width="{size}px"
         style:transform="skewY(-30deg) scaleX({cos30})"
       >
         Edu
-      </div></a
-    >
+      </div>
+    </a>
   </li>
 </div>
 
